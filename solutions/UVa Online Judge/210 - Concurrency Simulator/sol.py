eee7db90220da8c85dee06538408b09429033a8b
d1t2 <dieterplex@gmail.com>
2022-04-04 01:14:22 +0800

    210 fix

--- solutions/UVa Online Judge/210 - Concurrency Simulator/sol.py
+++ solutions/UVa Online Judge/210 - Concurrency Simulator/sol.py
@@ -2,32 +2,34 @@ from queue import Queue
 from collections import deque
 import sys  # debug print
 def d(msg):
-    sys.stderr.write(">>>>>>> " + msg + "\n")
+    print(">>>>>>> " + msg, file=sys.stderr)
 
-# load context
-cases = int(input())
-(pcount, t_asgn, t_print, t_lock, t_unlock, t_end, QUANT) = [
-    int(i) for i in input().split()
-]
-# load programs
-programs = list()
-for i in range(pcount):
-    lines = []
-    for instr in range(25):
-        line = input()
-        lines.append(line)
-        if line == "end":
-            break
-    programs.insert(i, lines)
-# import pprint; pprint.pprint(programs)
+def init():
+    global programs, var, lock, qblock, qwait, QUANT
+    global t_asgn, t_print, t_lock, t_unlock, t_end
+    # load context
+    (pcount, t_asgn, t_print, t_lock, t_unlock, t_end, QUANT) = [
+        int(i) for i in input().split()
+    ]
+    # load programs
+    programs = list()
+    for i in range(pcount):
+        lines = []
+        while True:
+            line = input()
+            lines.append(line)
+            if line == "end":
+                break
+        programs.insert(i, lines)
+    # import pprint; pprint.pprint(programs)
 
-# internal state
-var = {chr(97 + i): 0 for i in range(26)}
-lock = False
-qblock = Queue()
-qwait = deque()
-qwait.extendleft(range(pcount))
-# enqueue program id. [pcount-1, ..., 0]
+    # internal state
+    var = {chr(97 + i): 0 for i in range(26)}
+    lock = False
+    qblock = Queue()
+    qwait = deque()
+    qwait.extendleft(range(pcount))
+    # enqueue program id. [pcount-1, ..., 0]
 
 
 def exec_cmd(p, pid) -> int:
@@ -35,16 +37,14 @@ def exec_cmd(p, pid) -> int:
     global lock
     global qblock
     global qwait
-    excute: str = p.pop(0)
+    excute = p.pop(0)
     cmd = excute.split()
-    d(f"{pid}: {excute}")
+    d("{}: {}".format(pid, excute))
     if len(cmd) > 1 and cmd[1] == "=":
         var[cmd[0]] = int(cmd[2])
         return t_asgn
     elif cmd[0] == "print":
-        print(f"{pid+1}: {var[cmd[1]]}")
-        if pid == 1 and var[cmd[1]] == 8:
-            d("debug here")
+        print("{}: {}".format(pid+1, var[cmd[1]]))
         return t_print
     elif cmd[0] == "lock":
         if lock:
@@ -57,7 +57,7 @@ def exec_cmd(p, pid) -> int:
             return t_lock
     elif cmd[0] == "unlock":
         if not qblock.empty():
-            qwait.appendleft(qblock.get())
+            qwait.append(qblock.get())
         lock = False
         return t_unlock
     else:  # end
@@ -78,7 +78,13 @@ def exec_prog(prog, prog_id, quant):
     return True
 
 
-while len(qwait) > 0:
-    id = qwait.pop()
-    if exec_prog(programs, id, QUANT):
-        qwait.appendleft(id)
+T = int(input())
+for t in range(T):
+    input()
+    init()
+    while len(qwait) > 0:
+        id = qwait.pop()
+        if exec_prog(programs, id, QUANT):
+            qwait.appendleft(id)
+    if t != T - 1:
+        print()
